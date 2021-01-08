85ebbbfd88a0ca9b80d2e035204dbd1c6eb15c61
GitHub Action <action@github.com>
2021-01-08 20:05:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/ggascoigne/regexplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ggascoigne/regexplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggascoigne.regexplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ggascoigne.regexplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ggascoigne/regexplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ggascoigne/regexplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggascoigne.regexplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ggascoigne.regexplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
